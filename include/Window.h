44254d4232d8d6a91cc2db90a7a501ecee787cb1
agueo <lopez.agueo@gmail.com>
2025-02-28 17:10:17 -0500

    Removed window class. uneeded for now

--- include/Window.h
+++ include/Window.h
@@ -2,20 +2,10 @@
 #define WINDOW_H
 
 #include <memory>
+
 #include <SFML/Graphics.hpp>
 #include "common.h"
 #include "JoyPad.h"
-
-class Window {
-public:
-	Window();
-	~Window() = default;
-	void update();
-	void draw(const uint32_t *data);
-
-private:
-	sf::Window window;
-};
 
 void handle_key_pressed(sf::Event &event, std::shared_ptr<JoyPad> joypad);
 void handle_key_released(sf::Event &event, std::shared_ptr<JoyPad> joypad);
--- src/Window.cpp
+++ src/Window.cpp
@@ -1,15 +1,4 @@
-#include <array>
 #include "Window.h"
-
-Window::Window() {
-	window.create(sf::VideoMode(600, 800), "Microboy");
-}
-
-void Window::update() {
-}
-
-void Window::draw(const uint32_t *data) {
-}
 
 void handle_key_pressed(sf::Event &event, std::shared_ptr<JoyPad> joypad) {
 	switch (event.key.code) {
